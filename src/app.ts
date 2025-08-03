a872d777fc1b1a6b8a7389f3a7dcc916bc5f0da1
lucasb <lkab05@hotmail.com>
2025-08-03 12:23:43 -0300

    feat: implemented user registration with validation and error handling

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,30 @@
-import { PrismaClient } from '@prisma/client';
 import fastify from 'fastify';
+import { ZodError } from 'zod';
+import { env } from './env';
+import { appRoutes } from './http/routes';
 
 export const app = fastify();
 
-const prisma = new PrismaClient();
+const BAD_REQUEST_STATUS_CODE = 400;
+const INTERNAL_SERVER_ERROR_CODE = 500;
 
-prisma.user.create({
-  data: {
-    name: 'Lucas',
-    email: 'lucasb@email.com',
-  },
+app.register(appRoutes);
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(BAD_REQUEST_STATUS_CODE).send({
+      message: 'Validation error',
+      issues: error.format(),
+    });
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error);
+  } else {
+    // TODO: Enviar o log para uma ferramenta de observabilidade externa.
+  }
+
+  return reply.status(INTERNAL_SERVER_ERROR_CODE).send({
+    message: 'Internal server error.',
+  });
 });
