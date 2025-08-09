7e8f36b0234795ca5e461523a502bf711a5e2004
lucasb <lkab05@hotmail.com>
2025-08-09 20:53:42 -0300

    chore: configure Vitest with unit and e2e test environments using Prisma

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,43 @@
+import { prisma } from '@/lib/prisma';
+import { execSync } from 'node:child_process';
+import { randomUUID } from 'node:crypto';
+import { URL } from 'node:url';
+import { Environment } from 'vitest/environments';
+
+function generateDatabaseUrl(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL is not defined');
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+
+  url.searchParams.set('schema', schema);
+
+  return url.toString();
+}
+
+export default <Environment>{
+  name: 'prisma',
+  transformMode: 'ssr',
+  async setup() {
+    // create a new database schema for tests environment
+    const schema = randomUUID();
+    const databaseUrl = generateDatabaseUrl(schema);
+
+    console.log(databaseUrl);
+
+    process.env.DATABASE_URL = databaseUrl;
+
+    execSync('npx prisma migrate deploy');
+
+    return {
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`
+        );
+
+        await prisma.$disconnect();
+      },
+    };
+  },
+};
