c1c02413886c656f8e14366fa96b621a991052ca
lucasb <lkab05@hotmail.com>
2025-08-10 13:18:46 -0300

    wip

---
+++ src/http/controllers/users/routes.ts
@@ -0,0 +1,13 @@
+import { FastifyInstance } from 'fastify';
+import { authenticate } from './authenticate';
+import { profile } from './profile';
+import { register } from './register';
+import { verifyJWT } from '../../middlewares/verify-jwt';
+
+export async function userRoutes(app: FastifyInstance) {
+  app.post('/users', register);
+  app.post('/sessions', authenticate);
+
+  /** Authenticated Routes **/
+  app.get('/me', { onRequest: [verifyJWT] }, profile);
+}
